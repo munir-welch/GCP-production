9af4d35faba825f8f719eb0b95405d6a4215e087
Munir93 <37178013+Munir93@users.noreply.github.com>
2018-08-09 17:36:23 +0100

    Update requirements.py

--- twitterStreamingtoBigQuery-Code/requirements.py
+++ twitterStreamingtoBigQuery-Code/requirements.py
@@ -2,9 +2,7 @@ import os
 
 #os.system('sudo apt-get -y install python-pip')
 
-os.system('sudo pip install --upgrade tweepy')
 os.system('sudo pip install --upgrade google-cloud-pubsub')
-os.system('sudo pip install logging')
 os.system('sudo pip install apache-beam[gcp]')
 
 
