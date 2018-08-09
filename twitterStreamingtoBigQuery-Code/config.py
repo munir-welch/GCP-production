dc33ebc0f190aa5460b3dcf6ef3182d83c01a282
Munir93 <37178013+Munir93@users.noreply.github.com>
2018-08-09 16:46:48 +0100

    Update config.py

--- twitterStreamingtoBigQuery-Code/config.py
+++ twitterStreamingtoBigQuery-Code/config.py
@@ -1,34 +1,25 @@
 
+'''Twitter 2 PubSub Configuration'''
 
 
+#ENTER YOUR GCP PROJECT ID
 PROJECT_ID ='warm-airline-207713'
 
-
-#creating dataset
-
-DATASET = 'Tweets_raw'
-
-#google cloud storage info
-
+# ENTER YOUR GCP SOTRAGE BUCKET NAME
 BUCKET_NAME = 'dod-mwja-project1'
 
+#ENTER YOUR PUBSUB TOPIC NAME e.g 'twitter-stream'
+TOPIC_NAME = 
 
-'''static-migrator  i.e batch load'''
-
-
-STREAM_TABLE = 'Donald_Trump_Tweets_DS'
-
-'''Twitter ETL start STREAM '''
-
+#ENTER THE HASHTAG YOU WANT TO TRACK 
 TRACK_TERMS = "brexit"
 
+#ENTER YOUR TWITTER CREDENTIALS 
 TWITTER_APP_KEY = 'KPVEb25geZ1WUxMrJz3flS6je'
 TWITTER_APP_SECRET = 'BcjmYWEJmDuU81gwW3Hs64mnzH4I67EzgS0H5FfuIs7VFjkXtH'
 TWITTER_KEY = '1014592058215059456-Z1pGlZHdMvx8vsszkDWpsEdTCjfz4o'
 TWITTER_SECRET = 'Zsw17mh9OiXDfaf993njgHhGZHxYTHrrD9Ae8fgqEc1mq'
 
-
-
-COLUMN_NAMES = ['Username', 'Tweet', 'Time', 'Followers', 'Location', 'Source']
-
+#ENTER THE NAME OF THE BIQ QUERY TABLE YOUR STREAMING INTO
+STREAM_TABLE = 'Donald_Trump_Tweets_DS'
 
